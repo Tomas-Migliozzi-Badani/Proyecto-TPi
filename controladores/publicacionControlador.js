d8822af94b30a5cfc80bd6c0df525dcbc704c7c4
unknown <tomasmigliozzi@gmail.com>
2026-06-08 12:37:50 -0300

    avance con la eestructura de las publicaciones

--- controladores/publicacionControlador.js
+++ controladores/publicacionControlador.js
@@ -1,8 +1,60 @@
+const publicaciones = [
+    {
+        id:1 ,
+        titulo:"atardecer" , 
+        autor: "tomas" , 
+        categoria: "Naturaleza"
+    },
+    {
+        id:2 , 
+        titulo: "el best" , 
+        autor: "anda a saber",
+        categoria:"paisajes"
+    }
+];
 export const listarPublicaciones = (req,res) => {
-    res.render("publicaciones/listar");
+    res.render("publicaciones/listar" , { 
+        publicaciones
+    });
 };
 
 
 export const mostrarFormularioCrear = (req,res) => {
+    console.log("Entro en el formulario");
     res.render("publicaciones/crear");
+};
+
+export const crearPublicacion = (req,res) => {
+    
+    console.log(req.body);
+
+    const nuevaPublicacion = {
+        
+        id:publicaciones.length + 1 ,
+        titulo: req.body.titulo , 
+        autor: req.body.autor ,
+        descripcion: req.body.descripcion
+    
+    };
+    
+    publicaciones.push(nuevaPublicacion);
+
+    res.redirect("/publicaciones");
+};
+
+export const mostrarDetallePublicacion = (req,res) => {
+    const id = parseInt(req.params.id);
+
+    const publicacion = publicaciones.find(
+        p => p.id === id 
+    );
+    if(!publicacion){
+        return res.send("publicacion no encontrada");
+    }
+    res.render("publicaciones/detalle",{
+        publicacion
+    });
+};
+export const mostrarFormularioEditar = (req,res) =>{
+    res.render("publicaciones/editar");
 };
--- rutas/publicacionRouter.js
+++ rutas/publicacionRouter.js
@@ -1,17 +1,25 @@
 import express from "express";
 import{
     listarPublicaciones,
-    mostrarFormularioCrear
+    mostrarFormularioCrear,
+    crearPublicacion,
+    mostrarDetallePublicacion,
+    mostrarFormularioEditar
 } from "../controladores/publicacionControlador.js";
 
 
 
 const router = express.Router();
 
-
-
 router.get("/",listarPublicaciones);
 
-router.get("/crear",mostrarFormularioCrear);
 
+
+router.get("/crear", mostrarFormularioCrear);
+router.post("/crear",crearPublicacion);
+
+router.get("/:id/editar", mostrarFormularioEditar);
+//router.post("/:id/editar", editarPublicacion);
+
+router.get("/:id",mostrarDetallePublicacion);
 export default router;
